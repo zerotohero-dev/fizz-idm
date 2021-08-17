1d3c32d7ddd141850b672d3d33f537705ba64e19
Volkan Ozcelik <me@volkan.io>
2021-08-16 18:57:35 -0700

    run script

---
+++ hack/run-dev.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+#  \
+#  \\,
+#   \\\,^,.,,.                     Zero to Hero
+#   ,;7~((\))`;;,,               <zerotohero.dev>
+#   ,(@') ;)`))\;;',    stay up to date, be curious: learn
+#    )  . ),((  ))\;,
+#   /;`,,/7),)) )) )\,,
+#  (& )`   (,((,((;( ))\,
+
+# shellcheck disable=SC1090
+source ~/.zprofile
+
+go run cmd/main.go
