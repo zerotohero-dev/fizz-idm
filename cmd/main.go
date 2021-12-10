959062becd90d3d6f9852d3a247aaef52b0a1ee0
Volkan Ozcelik <me@volkan.io>
2021-12-09 22:14:18 -0800

    added inline code to test things

--- cmd/main.go
+++ cmd/main.go
@@ -12,8 +12,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/zerotohero-dev/fizz-app/pkg/app"
+	"github.com/zerotohero-dev/fizz-entity/pkg/reqres"
 	"github.com/zerotohero-dev/fizz-env/pkg/env"
 	"github.com/zerotohero-dev/fizz-idm/internal/api"
 	"github.com/zerotohero-dev/fizz-idm/internal/data"
@@ -37,43 +39,31 @@ func main() {
 	api.InitializeEndpoints(e, r)
 	app.RouteHealthEndpoints(e.Idm.PathPrefix, r)
 
-	//go func() {
+	// For demo only!
+	go func() {
+		value := "potato"
+		res, err := mtls.CryptoHashCreate(reqres.HashCreateRequest{
+			Value: value,
+		})
 
-	//}()
-	//
-	//func() {
-	//	res, err := mtls.CryptoJwtCreate(reqres.JwtCreateRequest{
-	//		Email: "me@volkan.io",
-	//	})
-	//
-	//	if err != nil {
-	//		log.Err("Error creating jwt: %s", err.Error())
-	//		return
-	//	}
-	//
-	//	if res == nil {
-	//		log.Err("nil response")
-	//		return
-	//	}
-	//
-	//	log.Info("verified: %s", res.Token)
-	//
-	//	vr, err := mtls.CryptoJwtVerify(reqres.JwtVerifyRequest{
-	//		Token: res.Token,
-	//	})
-	//
-	//	if err != nil {
-	//		log.Err("Error creating jwt: %s", err.Error())
-	//		return
-	//	}
-	//
-	//	if vr == nil {
-	//		log.Err("nil response")
-	//		return
-	//	}
-	//
-	//	log.Info("verified: %s %s %s", vr.Email, vr.Expires, vr.Valid)
-	//}()
+		if err != nil || res == nil {
+			return
+		}
+
+		fmt.Println("Generated hash:", res.Hash)
+
+		vr, err := mtls.CryptoHashVerify(reqres.HashVerifyRequest{
+			Value: value,
+			Hash:  res.Hash,
+		})
+
+		if err != nil || vr == nil {
+			return
+		}
+
+		fmt.Println("Verified:", vr.Verified)
+		fmt.Println("Done.")
+	}()
 
 	app.ListenAndServe(e, svcName, appEnv.Port, r)
 }
